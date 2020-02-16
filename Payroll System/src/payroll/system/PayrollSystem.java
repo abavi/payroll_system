e8a736d53f6a4f05fda3869e8ceba6f3b5f3dd12
Bavi <andreibavi@yahoo.com>
2020-02-16 12:03:04 +0000

    Added sub-menu functionality

--- Payroll System/src/payroll/system/PayrollSystem.java
+++ Payroll System/src/payroll/system/PayrollSystem.java
@@ -11,11 +11,42 @@ public class PayrollSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Printing the main menu on screen and allowing user input
         String choice;
         Scanner scanner = new Scanner(System.in);
-        printMainMenu();
-        System.out.print("Please enter your option: ");
-        choice = scanner.nextLine();
+        //TODO Move Menu functionality in a class
+        //TODO Add choice validation
+        do {
+            printMainMenu();
+            System.out.print("Please enter your option: ");
+            choice = scanner.nextLine();
+            //Allowing sub-menus to be printed
+            switch (choice) {
+                case "1":
+                    do{
+                        printRegisterMenu();
+                        System.out.print("Please enter your option: ");
+                        choice = scanner.nextLine();
+                    }while(!choice.equals("d")); //Going back to the main-menu
+                    break;
+                case "2":
+                    do{
+                        printViewMenu();
+                        System.out.print("Please enter your option: ");
+                        choice = scanner.nextLine();
+                    }while(!choice.equals("e")); //Going back to the main-menu
+                    break;
+                case "4":
+                    do{
+                        printPayslipMenu();
+                        System.out.print("Please enter your option: ");
+                        choice = scanner.nextLine();
+                    }while(!choice.equals("d")); //Going back to the main-menu
+                    break;
+                default:                    
+                    break;
+            }
+        } while (!choice.equals("5"));
     }
     
     // Method to print the initial main menu
@@ -32,4 +63,39 @@ public class PayrollSystem {
     
     //TODO Add sub-menus for 1 2 4
     
+    //Sub-menu for registering an employee to the system
+    public static void printRegisterMenu(){
+        System.out.println("***********************"
+                +           "\n*  Register Employee  *" 
+                +           "\n***********************"                
+                +           "\na. Salaried Employees"
+                +           "\nb. Hourly Employees"
+                +           "\nc. Commission Employees"
+                +           "\nd. Back");
+    }
+    
+    //Sub-menu for viewing an employee
+    public static void printViewMenu(){
+        System.out.println("********************"
+                +           "\n*  Payroll System  *" 
+                +           "\n********************"                
+                +           "\na. All Employees"
+                +           "\nb. Salaried Employees"
+                +           "\nc. Hourly Employees"
+                +           "\nd. Commission Employees"
+                +           "\ne. Back");
+    }
+    
+    //Sub-menu for generating pay-slips
+    public static void printPayslipMenu(){
+        System.out.println("********************"
+                +           "\n*  Payroll System  *" 
+                +           "\n********************"                
+                +           "\na. Salaried Employees"
+                +           "\nb. Hourly Employees"
+                +           "\nc. Commission Employees"
+                +           "\nd. Back");
+    }
 }
+    
+
